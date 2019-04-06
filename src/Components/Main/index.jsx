28d44702c1f93525d85ce16021ee0efd86616782
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-06 22:06:05 +0300

    add the search dynamic to the home page

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Search from "./../Search";
 import Movies from "./../Movies";
 import getTrends from "./../../utilis/getTrends";
+import getMovie from "./../../utilis/getMovie";
 import "./style.css";
 
 export default class extends Component {
@@ -9,20 +10,46 @@ export default class extends Component {
     super(props);
     this.state = {
       searchValue: null,
-      movies: []
+      movies: [],
+      userDidSearch: false
     };
     this.updateSearchValue = this.updateSearchValue.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
   }
-  updateSearchValue() {}
+  updateSearchValue(e) {
+    this.setState({ searchValue: e.target.value });
+    // this.state.searchValue = e.target.value;
+  }
+  submitSearch(e) {
+    e.preventDefault();
+    getMovie(this.state.searchValue).then(response => {
+      this.setState({
+        movies: response.results.slice(0, 10),
+        userDidSearch: true
+      });
+    });
+  }
   componentDidMount() {
     getTrends().then(response => {
-      this.setState({ movies: response.results.slice(0, 10) });
+      this.setState({
+        movies: response.results.slice(0, 10),
+        userDidSearch: false
+      });
     });
   }
   render() {
     return (
       <main className="main">
-        <Search />
+        <Search
+          updateSearchValue={this.updateSearchValue}
+          searchValue={this.state.searchValue}
+          submitSearch={this.submitSearch}
+        />
+        <div className="movies-type">
+          {this.state.userDidSearch
+            ? "Your Search Results"
+            : "Top Watched movies for this week"}
+        </div>
         <Movies movies={this.state.movies} />
       </main>
     );
