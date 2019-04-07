995d81cdacc9225bfb1bd6d1d83c35446d9aaf3c
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-07 22:03:22 +0300

    fix bugs

--- src/utilis/getMovie.js
+++ src/utilis/getMovie.js
@@ -1,7 +1,7 @@
 import { SEARCH_MOVIE_URL } from "../config";
 
-export default movieName =>
-  fetch(`${SEARCH_MOVIE_URL}${movieName}`)
+export default (movieName, genre, quality, rating) =>
+  fetch(`${SEARCH_MOVIE_URL}${movieName}${genre}${quality}${rating}`)
     .then(res => res.json())
     .catch(err => {
       throw err;
